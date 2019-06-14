6ebb12359a05ea24497b6d4f2b681c727fb0baf4
christofer <christofer.rodriguez@ucr.ac.cr>
2019-06-14 15:33:16 -0600

    Indice reader

---
+++ IndiceReader.py
@@ -0,0 +1,33 @@
+import re
+
+class IndiceReader:
+    def __init__(self, docName):
+        docName = str(docName)
+        self.fileName = './DocumentosProcesados/indice/' + docName + '.txt'
+        # Se lee el archivo indice
+        self.file = open(self.fileName, "r")
+        string = self.file.read()
+        self.fileLines = string.split("\n")
+
+    # Metodo que devuelve el archivo
+    def getLinesPostings(self):
+        return self.fileLines
+
+    # Metodo que devuelve un diccionario de Postings
+    # Llave: termino
+    # Valor: posinicial, cantEntradas
+    def getDicPostings(self):
+        indice = dict()
+        for line in self.fileLines:
+            lineWithoutSpace = re.sub(r'\s+', ' ', line)
+            lineDiv = lineWithoutSpace.split(" ")
+            if len(lineDiv) >= 3:
+                term = lineDiv[0]
+                indice[term] = int(lineDiv[1]), int(lineDiv[2])
+        return indice
+
+if __name__ == '__main__':
+    tokReader = IndiceReader('indice')
+    wtd = tokReader.getDicPostings()
+    for word in wtd:
+        print(str(word) + " " + str(wtd[word]))
